8695b0e53b508ec06f1c5e97af09c255b1732e0f
Matheus Reis <135277320+MatheusAyra@users.noreply.github.com>
2025-03-07 16:57:27 -0300

    organograma

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX - como o React trabalha de forma visual
+    return (
+        <header className="banner">
+          <img src="/imagens/banner.png" alt="banner principal"/>
+        </header>
+    )
+
+}
+
